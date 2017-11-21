826d8d3b9759e76a67d6f59c89eeabe5f81903d2
Abhishek Gupta <abhirockzz@gmail.com>
2017-11-21 20:21:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
-#!/bin/sh
-#enable proxy
-#export HTTP_PROXY=http://www-proxy.us.oracle.com:80
-#export HTTPS_PROXY=https://www-proxy.us.oracle.com:80
-
 go get github.com/bsm/sarama-cluster
 go get github.com/go-sql-driver/mysql
 go run gokafkamysql.go
